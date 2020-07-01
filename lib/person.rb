17c5ef41209991950463c86f763780a35e82bbaf
Skyler Littleaxe <littleaxe.skyler@gmail.com>
2020-07-01 02:14:32 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -4,4 +4,6 @@ attr_accessor :name, :birthday, :hair_color, :eye_color, :height,
 :wrist_size, :glove_size, :pant_length, :pant_width
 
 def initialize(attributes)
+   attributes.each {|key, value| self.send(("#{key}="), value)}
+ end
 end
